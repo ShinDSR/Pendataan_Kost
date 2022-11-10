83c62d897492044c2e757a98681a5c11943c56c1
AirelJavier <airel.javier.ft20@mail.umy.ac.id>
2022-11-10 20:57:11 +0700

    entitas type

---
+++ src/main/java/com/kost/data/kost/models/entities/type.java
@@ -0,0 +1,61 @@
+package com.kost.data.kost.models.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+@Table(name = "type")
+public class type implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public  char nama_type;
+
+    public char luas;
+
+    public type() {
+        
+    }
+
+    public type(Long id, char nama_type, char luas) {
+        this.id = id;
+        this.nama_type = nama_type;
+        this.luas = luas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public char getNama_type() {
+        return nama_type;
+    }
+
+    public void setNama_type(char nama_type) {
+        this.nama_type = nama_type;
+    }
+
+    public char getLuas() {
+        return luas;
+    }
+
+    public void setLuas(char luas) {
+        this.luas = luas;
+    }
+
+    
+    
+}
